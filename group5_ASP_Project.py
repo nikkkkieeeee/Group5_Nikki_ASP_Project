394ded5614dec7e96d8159c011c012a0387add0f
nikkkkieeeee <87739897+nikkkkieeeee@users.noreply.github.com>
2021-08-19 14:04:59 +0800

    1,2

--- group5_ASP_Project.py
+++ group5_ASP_Project.py
@@ -1,1 +1,21 @@
-sdfhgfg
+import pandas as pd
+country = pd.read_excel('IMVA.xls', sheet_name = "IMVA")
+print(country)
+
+print(country.columns)
+
+asia5 = country[['Periods','Brunei Darussalam','Indonesia','Malaysia','Myanmar','Philippines','Thailand','Vietnam','China','Hong Kong SAR','Taiwan','Japan','South Korea','Bangladesh','India','Pakistan','Sri Lanka','Iran','Israel','Kuwait','Saudi Arabia','United Arab Emirates']]
+print(asia5.columns)
+print(asia5)
+
+
+date = asia5['Periods'].str.split(' ', n = 1, expand = True)
+print(date)
+
+asia5 = asia5.assign(year=date[0])
+
+set1 = asia5[(asia5['year']) >= str(2011)]
+print(set1)
+
+print(set1.head(3))
+print(set1.tail(3))
