955773a496694c6236a691c63feb92476f0990dd
cecilia2002 <84916123+cecilia2002@users.noreply.github.com>
2021-08-19 16:54:01 +0800

    h

--- group5_ASP_Project.py
+++ group5_ASP_Project.py
@@ -19,3 +19,32 @@ print(set1)
 
 print(set1.head(3))
 print(set1.tail(3))
+
+set2 = set1[['Brunei Darussalam','Indonesia','Malaysia','Myanmar','Philippines','Thailand','Vietnam','China','Hong Kong SAR','Taiwan','Japan','South Korea','Bangladesh','India','Pakistan','Sri Lanka','Iran','Israel','Kuwait','Saudi Arabia','United Arab Emirates']]
+asia = set2.replace(',','', regex=True)
+asia111 = asia.replace('na','0', regex=True)
+print(asia)
+
+asia1 = asia111.astype(int)
+print(asia1.dtypes)
+psNotSorted=asia1.sum()
+print(psNotSorted)
+psSorted = psNotSorted.sort_values(ascending=False)
+print(psSorted)
+print(psSorted.head(3))
+print('The total no. of visitors for the top 3 countries is',sum(psSorted.head(3)))
+print('The mean value for the top 3 countries is',round(sum(psSorted.head(3))/len(psSorted.head(3)),2))
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ps = psNotSorted.sort_values(ascending=False)
+index = np.arange(len(ps.index))
+plt.bar(ps.index, ps.values/2500)
+plt.xlabel("Countries", fontsize=10)
+plt.ylabel("No. of Travellers (In thousands)", fontsize=10)
+plt.xticks(index, ps.index, fontsize=10, rotation=80)
+plt.title("All other countries from (Period:2011 - 2020 ", size=12)
+plt.show();
+
+
