ec75b972d60d3413a04b273df254597618960c91
cecilia2002 <84916123+cecilia2002@users.noreply.github.com>
2021-08-19 14:05:27 +0800

    h

--- group5_ASP_Project.py
+++ group5_ASP_Project.py
@@ -1,1 +1,20 @@
-sdfhgfg
+import pandas as pd
+data = pd.read_excel('IMVA.xls', sheet_name = 2)
+print(data)
+
+data.columns
+print(data.columns)
+
+asia = data['Brunei Darussalam', 'Indonesia', 'Malaysia', 'Myanmar', 'Philippines', 'Thailand', 'Vietnam', 'Other Markets In Southeast Asia', 'Greater China', 'China', 'Hong Kong SAR', 'Taiwan', 'Other Markets In Greater China', 'North Asia', 'Japan', 'South Korea', 'Other Markets In North Asia', 'South Asia', 'Bangladesh', 'India', 'Pakistan', 'Sri Lanka', 'Other Markets In South Asia', 'West Asia', 'Iran', 'Israel', 'Kuwait', 'Saudi Arabia', 'United Arab Emirates']
+print(data.columns)
+
+asia = data(['Periods']).str.split
+
+data.head(3)
+print(data.head)
+
+
+
+
+
+
